a345c102931725c34045ba60ca6bf990c52c76c7
kushtrimhaziri <kushtrimhaziri@hotmail.com>
2024-08-05 23:34:26 +0200

    solution for day 1 year: 2016

--- internal/executor.go
+++ internal/executor.go
@@ -1,65 +1,70 @@
 package internal
 
 import (
-	"github.com/kushtrimhaziri/advent_of_code_go/years/2022/day_01"
-	"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_0"
-	"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_02"
-	"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_03"
-	"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_04"
-	"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_05"
-	"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_06"
-	"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_07"
-	"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_08"
-	"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_09"
-	"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_10"
-	"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_11"
-	"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_12"
-	"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_13"
-	"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_14"
-	"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_15"
-	"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_16"
-	"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_17"
-	"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_18"
-	"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_19"
-	"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_20"
-	"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_21"
-	"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_22"
-	"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_23"
-	"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_24"
-	"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_25"
+	"github.com/kushtrimhaziri/advent_of_code_go/years/2016/day_01"
+	//"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_0"
+	//"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_02"
+	//"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_03"
+	//"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_04"
+	//"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_05"
+	//"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_06"
+	//"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_07"
+	//"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_08"
+	//"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_09"
+	//"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_10"
+	//"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_11"
+	//"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_12"
+	//"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_13"
+	//"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_14"
+	//"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_15"
+	//"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_16"
+	//"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_17"
+	//"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_18"
+	//"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_19"
+	//"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_20"
+	//"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_21"
+	//"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_22"
+	//"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_23"
+	//"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_24"
+	//"github.com/kushtrimhaziri/advent_of_code_go/years/2023/day_25"
 )
 
 // RunChallenge executes the challenge of a specific day with the provided input.
 func RunChallenge(day int, inputPath string, mode int) {
-	input := LoadInputLines(inputPath)
-	mapping := map[int]func([]string, int){
-		0:  day_0.Run,
-		1:  day_01.Run,
-		2:  day_02.Run,
-		3:  day_03.Run,
-		4:  day_04.Run,
-		5:  day_05.Run,
-		6:  day_06.Run,
-		7:  day_07.Run,
-		8:  day_08.Run,
-		9:  day_09.Run,
-		10: day_10.Run,
-		11: day_11.Run,
-		12: day_12.Run,
-		13: day_13.Run,
-		14: day_14.Run,
-		15: day_15.Run,
-		16: day_16.Run,
-		17: day_17.Run,
-		18: day_18.Run,
-		19: day_19.Run,
-		20: day_20.Run,
-		21: day_21.Run,
-		22: day_22.Run,
-		23: day_23.Run,
-		24: day_24.Run,
-		25: day_25.Run,
-	}
+	inputFirstinputLine := LoadFirstInputLine(inputPath)
 
-	mapping[day](input, mode)
+	mappingForFirstLine := map[int]func(string, int){
+		1: day_01.Run,
+	}
+	//input := LoadInputLines(inputPath)
+	//mapping := map[int]func([]string, int){
+	//	0:  day_0.Run,
+	//	2:  day_02.Run,
+	//	3:  day_03.Run,
+	//	4:  day_04.Run,
+	//	5:  day_05.Run,
+	//	6:  day_06.Run,
+	//	7:  day_07.Run,
+	//	8:  day_08.Run,
+	//	9:  day_09.Run,
+	//	10: day_10.Run,
+	//	11: day_11.Run,
+	//	12: day_12.Run,
+	//	13: day_13.Run,
+	//	14: day_14.Run,
+	//	15: day_15.Run,
+	//	16: day_16.Run,
+	//	17: day_17.Run,
+	//	18: day_18.Run,
+	//	19: day_19.Run,
+	//	20: day_20.Run,
+	//	21: day_21.Run,
+	//	22: day_22.Run,
+	//	23: day_23.Run,
+	//	24: day_24.Run,
+	//	25: day_25.Run,
+	//}
+
+	mappingForFirstLine[day](inputFirstinputLine, mode)
+	//mapping[day](input, mode)
 }
--- years/2016/day_01/main.go
+++ years/2016/day_01/main.go
@@ -2,10 +2,55 @@ package day_01
 
 import (
 	"fmt"
+	"math"
+	"strconv"
+	"strings"
 )
 
+type Direction int
+
+const (
+	Up Direction = iota
+	Down
+	Left
+	Right
+)
+
+type PointWithCoordinatesOnly struct {
+	X, Y int
+}
+
+type Point struct {
+	X, Y int
+	Direction
+}
+
+type UpdatedPoint struct {
+	Point  Point
+	Exists bool
+}
+
+var directionChanges = map[Direction]map[string]Point{
+	Up: {
+		"R": {X: 1, Y: 0, Direction: Right},
+		"L": {X: -1, Y: 0, Direction: Left},
+	},
+	Down: {
+		"R": {X: -1, Y: 0, Direction: Left},
+		"L": {X: 1, Y: 0, Direction: Right},
+	},
+	Left: {
+		"R": {X: 0, Y: 1, Direction: Up},
+		"L": {X: 0, Y: -1, Direction: Down},
+	},
+	Right: {
+		"R": {X: 0, Y: -1, Direction: Down},
+		"L": {X: 0, Y: 1, Direction: Up},
+	},
+}
+
 // Run function of the daily challenge
-func Run(input []string, mode int) {
+func Run(input string, mode int) {
 	if mode == 1 || mode == 3 {
 		fmt.Printf("Part one: %v\n", Part1(input))
 	}
@@ -15,11 +60,107 @@ func Run(input []string, mode int) {
 }
 
 // Part1 solves the first part of the exercise
-func Part1(input []string) string {
-	return ""
+func Part1(input string) string {
+	commands := parseCommands(input)
+	point := Point{X: 0, Y: 0, Direction: Up}
+
+	for _, cmd := range commands {
+		point = updatePointForPart1(point, point.Direction, cmd.command, cmd.amount)
+	}
+
+	sumOfCommands := math.Abs(float64(point.X)) + math.Abs(float64(point.Y))
+
+	return strconv.FormatFloat(sumOfCommands, 'f', -1, 64)
 }
 
 // Part2 solves the second part of the exercise
-func Part2(input []string) string {
+func Part2(input string) string {
+	commands := parseCommands(input)
+	point := Point{X: 0, Y: 0, Direction: Up}
+	pointMap := make(map[PointWithCoordinatesOnly]bool)
+	exists := false
+	updatedPoint := UpdatedPoint{point, exists}
+
+	for _, cmd := range commands {
+		updatedPoint = updatePointForPart2(updatedPoint, updatedPoint.Point.Direction, cmd.command, cmd.amount, pointMap)
+
+		if updatedPoint.Exists {
+			sumOfCommands := math.Abs(float64(updatedPoint.Point.X)) + math.Abs(float64(updatedPoint.Point.Y))
+
+			return strconv.FormatFloat(sumOfCommands, 'f', -1, 64)
+		}
+	}
 	return ""
+}
+
+func updatePointForPart1(currentPoint Point, direction Direction, command string, amount int) Point {
+	change := directionChanges[direction][command]
+
+	if change.X != 0 {
+		currentPoint.X += change.X * amount
+	} else {
+		currentPoint.Y += change.Y * amount
+
+	}
+	currentPoint.Direction = change.Direction
+
+	return currentPoint
+}
+
+func updatePointForPart2(currentPoint UpdatedPoint, direction Direction, command string, amount int, pointMap map[PointWithCoordinatesOnly]bool) UpdatedPoint {
+	change := directionChanges[direction][command]
+
+	if change.X != 0 {
+		for i := 0; i < amount; i++ {
+			currentPoint.Point.X += change.X * 1
+			newPoint := PointWithCoordinatesOnly{X: currentPoint.Point.X, Y: currentPoint.Point.Y}
+			if _, existsNewPoint := pointMap[newPoint]; existsNewPoint {
+				currentPoint.Exists = true
+				return currentPoint
+			}
+			pointMap[newPoint] = true
+		}
+	} else {
+		for i := 0; i < amount; i++ {
+			currentPoint.Point.Y += change.Y * 1
+			newPoint := PointWithCoordinatesOnly{X: currentPoint.Point.X, Y: currentPoint.Point.Y}
+
+			if _, existsNewPoint := pointMap[newPoint]; existsNewPoint {
+				currentPoint.Exists = true
+				return currentPoint
+			}
+			pointMap[newPoint] = true
+		}
+	}
+
+	currentPoint.Point.Direction = change.Direction
+	return currentPoint
+}
+func parseCommands(commandsStr string) []struct {
+	command string
+	amount  int
+} {
+	commandSegments := strings.Split(commandsStr, ", ")
+
+	parsedCommands := make([]struct {
+		command string
+		amount  int
+	}, len(commandSegments))
+
+	for i, segment := range commandSegments {
+		command := segment[:1]
+
+		amount, err := strconv.Atoi(segment[1:])
+		if err != nil {
+			fmt.Printf("Error parsing amount from %s: %v\n", segment, err)
+			continue
+		}
+
+		parsedCommands[i] = struct {
+			command string
+			amount  int
+		}{command, amount}
+	}
+
+	return parsedCommands
 }
